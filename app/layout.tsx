4f6218afce1f637c201ad6a6a653134d21e27155
Nathaniel Tampus <nate@mugnavo.com>
2024-06-18 19:54:16 +0800

    init

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+
+import { cn } from "~/lib/utils";
+import "~/styles/globals.css";
+
+const fontSans = FontSans({
+	subsets: ["latin"],
+	variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+	title: "HireHub",
+	description: "HireHub is a platform for hiring and job searching.",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body
+				className={cn(
+					"min-h-screen bg-background font-sans antialiased",
+					fontSans.variable
+				)}
+			>
+				{children}
+			</body>
+		</html>
+	);
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "~/components/ui/button";
+
+export default function Home() {
+	return (
+		<main className="flex flex-col gap-4 p-8">
+			Hello
+			<Button>World</Button>
+		</main>
+	);
+}
