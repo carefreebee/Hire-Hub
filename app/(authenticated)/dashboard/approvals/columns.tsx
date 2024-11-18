91eafb5934e04cca69e2fbc383e013a511f42cc9
lninyow <lidennino.sagmon@cit.edu>
2024-11-18 12:58:44 +0800

    job request approval wip

---
+++ app/(authenticated)/dashboard/approvals/columns.tsx
@@ -0,0 +1,149 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
+import { Button } from "~/components/ui/button";
+
+import { formatDate } from "~/lib/date-time";
+import { JobRequestSelect } from "~/lib/schema";
+import IDColumn from "./id-column";
+
+export const columns: ColumnDef<JobRequestSelect>[] = [
+	{
+		accessorKey: "request_id",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+				>
+					Request ID
+					<ArrowUpDown className="ml-2 h-4 w-4" />
+				</Button>
+			);
+		},
+		cell: ({ row }) => {
+			return (
+				<div className="flex items-center justify-center gap-2">
+					{row.getValue("request_id")}
+				</div>
+				// return <div className="flex items-center justify-center gap-2">{row.index + 1}</div>;
+			);
+		},
+	},
+	{
+		accessorKey: "first_name",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+				>
+					Name
+					<ArrowUpDown className="ml-2 h-4 w-4" />
+				</Button>
+			);
+		},
+		cell: ({ row }) => {
+			return (
+				<div className="flex items-center justify-center gap-2">
+					{row.getValue("first_name")} {row.getValue("last_name")}
+				</div>
+			);
+		},
+	},
+	{
+		accessorKey: "last_name",
+		header: () => {
+			return <div className="hidden p-0"></div>;
+		},
+		cell: () => {
+			return <div className="hidden p-0"></div>;
+		},
+	},
+	{
+		accessorKey: "requested_position",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+				>
+					Position
+					<ArrowUpDown className="ml-2 h-4 w-4" />
+				</Button>
+			);
+		},
+		cell: ({ row }) => {
+			return (
+				<div className="flex items-center justify-center gap-2">
+					{row.getValue("requested_position")}
+				</div>
+			);
+		},
+	},
+	{
+		accessorKey: "requested_department",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+				>
+					Department/Office
+					<ArrowUpDown className="ml-2 h-4 w-4" />
+				</Button>
+			);
+		},
+		cell: ({ row }) => {
+			return (
+				<div className="flex items-center justify-center gap-2">
+					{row.getValue("requested_department")}
+					{row.getValue("requested_office")}
+				</div>
+			);
+		},
+	},
+	{
+		accessorKey: "requested_office",
+		header: () => {
+			return <div className="hidden"></div>;
+		},
+		cell: () => {
+			return <div className="hidden"></div>;
+		},
+	},
+	{
+		accessorKey: "requested_date",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+				>
+					Request Date
+					<ArrowUpDown className="ml-2 h-4 w-4" />
+				</Button>
+			);
+		},
+		cell: ({ row }) => {
+			const date: Date = row.getValue("requested_date");
+			return <div className="flex items-center justify-center gap-2">{formatDate(date)}</div>;
+		},
+	},
+	{
+		id: "actions",
+		accessorKey: "Action",
+		header: () => {
+			return <div className="px-5">Action</div>;
+		},
+		cell: ({ row }) => {
+			const id: number = row.getValue("id");
+			return (
+				<div className="flex items-center justify-center">
+					<IDColumn id={id} />
+				</div>
+			);
+		},
+	},
+];
--- app/(authenticated)/dashboard/approvals/page.tsx
+++ app/(authenticated)/dashboard/approvals/page.tsx
@@ -1,3 +1,38 @@
-export default function Approvals() {
-	return <div></div>;
+import TypographyH4 from "~/components/ui/typography-h4";
+import { getAllJobRequest, getJobReqByDeptOrOffice } from "~/controller/JobRequestController";
+import { validateRequest } from "~/lib/auth";
+import { JobRequestSelect, RoleEnumsType } from "~/lib/schema";
+import { columns } from "./columns";
+import { DataTable } from "./data-table";
+
+export default async function Approvals() {
+	const { user } = await validateRequest();
+
+	const higherUps: RoleEnumsType[] = [
+		"guidance_center_staff",
+		"recruitment_officer",
+		"hr_head",
+		"vp_acad_affairs",
+		"vp_administration",
+		"univ_president",
+	];
+
+	let applicant: JobRequestSelect[] = [];
+
+	if (higherUps.includes(user?.role as RoleEnumsType)) {
+		applicant = await getAllJobRequest();
+	} else {
+		applicant = await getJobReqByDeptOrOffice(
+			user?.department_id as number,
+			user?.office_id as number
+		);
+	}
+	return (
+		<section className="bg-slate-200/30 px-10 py-10">
+			<TypographyH4 text="Job Request List" />
+			<div className="container mx-auto px-10">
+				<DataTable columns={columns} data={applicant} />
+			</div>
+		</section>
+	);
 }
