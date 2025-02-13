6a35f4fb5988415e20eb69cfd7f6134f073beab9
Kimberly <intingkimberly20.com>
2025-02-13 22:04:36 +0800

    correc t data to show in pie charts

--- components/pages/authenticated/dashboard/SecondPieChart.tsx
+++ components/pages/authenticated/dashboard/SecondPieChart.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ArrowDown } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Cell, Pie, PieChart as RechartsPieChart, ResponsiveContainer, Tooltip } from "recharts";
 import { Button } from "~/components/ui/button";
 import {
@@ -10,17 +10,34 @@ import {
 	DropdownMenuItem,
 	DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
-import { COLORSEXT } from "./FirstPieChart";
+import { COLORS } from "./FirstPieChart";
 
-const COLORS = [
-	"#FF5733", // Red
-	"#33FF57", // Green
-	"#3357FF", // Blue
-	"#FF33A1", // Pink
-	"#FF8C33", // Orange
-	"#33FFF5", // Cyan
-	"#8C33FF", // Purple
-	"#FF3333", // Dark Red
+const offices = [
+	"Executive Office",
+	"Center for Communications, Creatives, and Marketing",
+	"Office of the Management Information System",
+	"Center for e-Learning, and Technology Education",
+	"Human Resource Department",
+	"Quality Assurance Office for Administration",
+	"Student Success Office",
+	"Finance and Accounting Office",
+	"Office of the Property Custodian",
+	"Athletics Office",
+	"Office of the Community Extension Services",
+	"Central Visayas Food Innovation Center",
+	"Enrollment Technical Office / Information Systems Development",
+	"Guidance Center",
+	"Instructional Materials and Publications Office",
+	"Innovation and Technology Support Office",
+	"Medical & Dental Clinic",
+	"Makerspace",
+	"Multimedia Solutions and Documentation Office",
+	"Networking and Linkages Office",
+	"Research and Development Coordinating Office",
+	"Alumni Affairs Office",
+	"Technology Support Group",
+	"Wildcat Innovation Labs",
+	"Network of External University Support Services (NEXUSS)",
 ];
 
 interface PieChartProps {
@@ -41,20 +58,51 @@ const renderLegend = (props: any) => {
 	);
 };
 
-export default function FirstPieChart({ data }: PieChartProps) {
-	const dataLabel = data.map((d) => d.label);
-	const total = data.reduce((a, b) => a + b.value, 0) || 0;
-	const payload = data.map((d, i) => ({
-		value: d.label,
-		color: COLORSEXT[i % COLORSEXT.length],
-	}));
-	const [expanded, setExpanded] = useState(false);
+export default function FirstPieChart({ data, alldata }: any) {
+	const dataAlll = useMemo(()=>Object.entries(alldata)
+		.map(([stage, data]) => ({
+			label: stage,
+			value: (data as any).total,
+		}))
+		.filter((item) => item.value !== 0), [alldata]);
+	const totalAlll = useMemo(()=>dataAlll.reduce((a: any, b: any) => a + b.value, 0) || 0, [dataAlll]);
 
-	// Toggle the expansion/collapse
-	const toggleLegend = () => {
-		setExpanded(!expanded);
-	};
-	const [selectedOffice, setSelectedOffice] = useState(0);
+	const [dataAll, setDataAll] = useState(dataAlll);
+	const [totalAll, setTotalAll] = useState(totalAlll);
+	const [selectedOffice, setSelectedOffice] = useState("All");
+
+	const payload = Object.entries(alldata).map(([stage, data], i) => ({
+		value: stage
+			.split("_")
+			.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+			.join(" "),
+		color: COLORS[i % COLORS.length],
+	}));
+
+	useEffect(() => {
+		if (selectedOffice === "All") {
+			setDataAll(dataAlll);
+			setTotalAll(totalAlll);
+		} else {
+			if (!data[selectedOffice]) {
+				setDataAll([]);
+				setTotalAll(0);
+				return;
+			}
+
+			const dataByOffice = Object.entries(data[selectedOffice].stages)
+				.map(([stage, data]) => ({
+					label: stage,
+					value: data,
+				}))
+				.filter((item) => item.value !== 0);
+
+			setDataAll(dataByOffice);
+			setTotalAll(data[selectedOffice].total);
+		}
+	}, [selectedOffice, data, dataAlll, totalAlll]);
+
+	console.log(data, alldata);
 	return (
 		<>
 			<div className="relative flex items-center justify-between px-10 pr-14">
@@ -66,11 +114,11 @@ export default function FirstPieChart({ data }: PieChartProps) {
 							variant={"ghost"}
 							className="flex items-center gap-2 text-slate-500"
 						>
-							Choose Office <ArrowDown size={15} />
+							Filter by Office <ArrowDown size={15} />
 						</Button>
 					</DropdownMenuTrigger>
-					<DropdownMenuContent className="ml-[30%] flex max-h-56 w-fit flex-col gap-1 overflow-y-auto p-5 text-xs">
-						{["All", ...dataLabel].map((office, index: number) => (
+					<DropdownMenuContent className="-ml-[30%] items-end flex max-h-56 w-fit flex-col gap-1 overflow-y-auto p-5 text-xs">
+						{["All", ...offices].map((office, index: number) => (
 							<DropdownMenuItem
 								key={index}
 								className="my-0 flex w-fit flex-wrap break-words rounded-full p-0"
@@ -78,9 +126,9 @@ export default function FirstPieChart({ data }: PieChartProps) {
 								<Button
 									variant={"ghost"}
 									onClick={() => {
-										setSelectedOffice(index);
+										setSelectedOffice(office);
 									}}
-									className={`${selectedOffice == index ? "bg-red-800 text-white hover:bg-red-800" : " "} my-0 flex w-fit flex-wrap break-words rounded-full p-3`}
+									className={`${selectedOffice === office ? "bg-red-800 text-white hover:bg-red-800" : " "} my-0 flex w-fit flex-wrap break-words rounded-full p-3`}
 								>
 									{office}
 								</Button>
@@ -89,39 +137,29 @@ export default function FirstPieChart({ data }: PieChartProps) {
 					</DropdownMenuContent>
 				</DropdownMenu>
 			</div>
-			<div
-				className={`${expanded ? "items-start" : "items-end"} flex justify-center bg-white`}
-			>
+			<div className={`flex items-end justify-center bg-white`}>
 				<div className="bg-gray-100 rounded-md p-2">
-					{/* Toggle Button */}
-
-					{/* Legend Items */}
-					<div
-						className={`${expanded ? "" : "max-h-68 flex flex-col gap-1 overflow-y-auto"}`}
-					>
-						{payload.slice(0, expanded ? payload.length : 4).map((entry, index) => (
+					<div className={`max-h-68 flex flex-col gap-1 overflow-y-auto`}>
+						{payload.map((entry, index) => (
 							<div
 								key={`item-${index}`}
-								className="mb-1 flex w-60 items-center space-x-2 rounded bg-slate-100 p-2"
+								className="mb-1 flex w-fit items-center space-x-2 rounded bg-slate-100 p-2"
 							>
 								<div
 									className="h-4 w-4 rounded"
 									style={{ backgroundColor: entry.color }}
 								></div>
-								<span className="text-gray-800 w-56 flex-wrap text-xs">
+								<span className="text-gray-800 w-fit flex-wrap text-xs">
 									{entry.value}
 								</span>
 							</div>
 						))}
-						<button className="mb-2 text-sm text-blue-500" onClick={toggleLegend}>
-							{expanded ? "Show Less" : "Show More"}
-						</button>
 					</div>
 				</div>
 				<ResponsiveContainer width="50%" height={300}>
 					<RechartsPieChart>
 						<Pie
-							data={data}
+							data={dataAll}
 							dataKey="value"
 							nameKey="label"
 							cx="50%"
@@ -131,11 +169,8 @@ export default function FirstPieChart({ data }: PieChartProps) {
 							fill="#8884d8"
 							paddingAngle={5}
 						>
-							{data.map((entry, index) => (
-								<Cell
-									key={`cell-${index}`}
-									fill={COLORSEXT[index % COLORS.length]}
-								/>
+							{dataAll?.map((entry, index) => (
+								<Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
 							))}
 						</Pie>
 						<Tooltip />
@@ -149,7 +184,7 @@ export default function FirstPieChart({ data }: PieChartProps) {
 							fill="#333"
 						>
 							<tspan x="50%" dy="-20">
-								{selectedOffice - 1 === 0 ? total : data[selectedOffice].value}
+								{totalAll}
 							</tspan>
 							<tspan x="50%" dy="30">
 								applicants
@@ -157,26 +192,6 @@ export default function FirstPieChart({ data }: PieChartProps) {
 						</text>
 					</RechartsPieChart>
 				</ResponsiveContainer>
-				{/* <ResponsiveContainer width="100%" height={500}>
-					<RechartsPieChart>
-						<Pie
-							data={data}
-							dataKey="value"
-							nameKey="label"
-							cx="50%"
-							cy="50%"
-							outerRadius={150}
-							fill="#8884d8"
-							label
-						>
-							{data.map((entry, index) => (
-								<Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-							))}
-						</Pie>
-						<Tooltip />
-						<Legend content={renderLegend} />
-					</RechartsPieChart>
-				</ResponsiveContainer> */}
 			</div>
 		</>
 	);
